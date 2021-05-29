3f91002ae5be6b717b173eb9fee1d0d8dda4404a
Sam Friedman <samtx@outlook.com>
2021-05-28 19:44:31 -0500

    merge updates from venmo feature branch

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+import pytest
+
+from lunchmoney.utils import isodatestr_to_date
+
+@pytest.mark.parametrize("input_str, output_date", [
+    ('2020-12-09T03:03:49', date(2020, 12, 9)),
+    ('2020-12-10T16:00:26', date(2020, 12, 10)),
+    ('2020-12-28T22:43:05', date(2020, 12, 28)),
+    ('2020-12-28T22:59:33', date(2020, 12, 28)),
+    ('2021-01-24T21:30:15', date(2021, 1, 24)),
+    ('2021-02-01T17:18:15', date(2021, 2, 1)),
+])
+def test_isodatestr_to_date(input_str, output_date):
+    x = isodatestr_to_date(input_str)
+    assert x == output_date
---
+++ tests/test_venmo.py
@@ -0,0 +1,15 @@
+import pytest
+
+from lunchmoney.venmo import parse_venmo_currency_str
+
+@pytest.mark.parametrize("input_str, output_float", [
+    ('"- $2,500.00"', -2500.0),
+    ('+ $72.00', 72.0),
+    ('- $637.86', -637.86),
+    ('"+ $1,214.17"', 1214.17),
+    ('- $10.00', -10.0),
+    ('+ $10.00', 10.0),
+])
+def test_parse_venmo_currency_str(input_str, output_float):
+    x = parse_venmo_currency_str(input_str)
+    assert x == pytest.approx(output_float)
